b6251459973cdbfa0b9e682d74117c425dd22fc9
Joe <joedsonfera@hotmail.com>
2023-11-05 00:35:35 -0300

    fix: accordion de pedidos implementado

--- src/pages/Pedidos/Pedidos.jsx
+++ src/pages/Pedidos/Pedidos.jsx
@@ -1,146 +1,145 @@
-import { useEffect, useState, useContext } from "react"
-import { api } from "../../api/api"
+import React, { useEffect, useState, useContext } from "react";
+import { api } from "../../api/api";
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 import { UserContext } from "../../context/UserContext";
-import CardProduto from "../../components/CardProduto/CardProduto"
-import { Grid, GridItem, Text } from '@chakra-ui/react'
+import { Grid, GridItem, Text } from "@chakra-ui/react";
 import { GeralContext } from "../../context/GeralContext";
 import {
-    Accordion,
-    AccordionItem,
-    AccordionButton,    
-    AccordionPanel,
-    AccordionIcon,
-    Box,
-    Table,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableContainer,
-  } from '@chakra-ui/react'
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 
 const Pedidos = () => {
-    const { nome, idUser, user } = useContext(UserContext)
-    const {pedidos, setPedidos} = useContext(GeralContext)
-    const { id } = useParams();
+  const { nome, idUser, user } = useContext(UserContext);
+  const { pedidos, setPedidos } = useContext(GeralContext);
+  const { id } = useParams();
 
-    // const [pedidos, setPedidos] = useState([])
-    const [produtos, setProdutos] = useState([])
+  const [isAccordionExpanded, setIsAccordionExpanded] = useState(null);
+  const [produtos, setProdutos] = useState([]);
 
-    const getPedidos = async () => {
-        try {
-          const response = await api.get(`/pedidos/`);
-          console.log(response)
-          if (response.status === 200) {
-            console.log('user:', user);
-            console.log('Response data:', response.data)
-            const aux = response.data.filter((pedido) => pedido.idUser === user.id);
-            console.log('Filtered pedidos:', aux)
-            setPedidos(aux);
-          } else {
-            console.error("Erro ao obter a lista de pedidos.");
-          }
-        } catch (error) {
-          console.error("Erro ao obter a lista de pedidos:", error);
-        }
-    };      
+  const handleAccordionClick = (index) => {
+    setIsAccordionExpanded(index);
+  };
 
-    const getProdutos = async () => {
-        const response = await api.get('/produtos')
-        setProdutos(response.data)
-        //console.log(produtos)
+  const getPedidos = async () => {
+    try {
+      const response = await api.get(`/pedidos/`);
+      if (response.status === 200) {
+        const aux = response.data.filter((pedido) => pedido.idUser === user.id);
+        setPedidos(aux);
+      } else {
+        console.error("Erro ao obter a lista de pedidos.");
+      }
+    } catch (error) {
+      console.error("Erro ao obter a lista de pedidos:", error);
     }
+  };
 
-    useEffect(() => {
-        getPedidos()
-        getProdutos()
-    }, [])
+  const getProdutos = async () => {
+    const response = await api.get("/produtos");
+    setProdutos(response.data);
+  };
 
-    return (
-        <div style={{ minheight:'100vh' }}>
-        <NavBar />
-        <Grid
-            templateRows={`repeat(${pedidos.length}, 1fr)`}
-            gap={4}
-            >
-            <Text mt={'1.4rem'} justifySelf={"flex-start"} fontSize={'2rem'} ><strong>{`${user.nome}`}</strong></Text>
-            {pedidos.map(({ id, valorTotal, idUser, itens }) => (
-                    <div key={id}>                   
-                        <Accordion defaultIndex={[1]} allowMultiple>
-                            <AccordionItem>
-                                <h2>
-                                <AccordionButton style={{ height: '60px', display: 'flex' }} >
-                                <Box as="span" flex="1" textAlign="left">
-                                    Pedido Nº {id}
-                                </Box>
-                                <Box as="span" flex="1" textAlign="center">
-                                    Forma de pagamento:
-                                </Box>
-                                <Box as="span" flex="1" textAlign="right">
-                                    Detalhes do pedido
-                                </Box>
-                                    <AccordionIcon />
-                                </AccordionButton>
-                                </h2>
-                                <AccordionPanel pb={4}>
-                                {itens.map(({ idProduto, quantidade }) => {                      
-                                    const produto = produtos.find((produto) => produto.id === idProduto);
+  useEffect(() => {
+    getPedidos();
+    getProdutos();
+  }, []);
 
-                                    if (produto) {
-                                        return (
-                                            <Grid
-                                                h='150px'
-                                                templateRows='repeat(2, 1fr)'
-                                                templateColumns='repeat(5, 1fr)'
-                                                marginBottom='10px'
-                                                gap={3}
-                                                >
-                                                <GridItem rowSpan={2} colSpan={1}>
-                                                    <img
-                                                    src={produto.imgurl}
-                                                    alt='Green double couch with wooden legs'
-                                                    borderRadius='sm'
-                                                    width={'80px'}
-                                                    />
-                                                </GridItem>
-                                                <GridItem colSpan={4} textAlign="left">
-                                                    <h2>{produto.nome}</h2>
-                                                </GridItem>
-                                                <GridItem colSpan={4} textAlign="right">
-                                                    <h2>R$ {produto.preco}</h2>
-                                                </GridItem>
-                                            </Grid>                                           
-                                        );
-                                    }
-                                    return null;
-                                })} 
-                                    <TableContainer>
-                                    <Table>
-                                        <Tbody>
-                                            <Tr>
-                                                <Td>TOTAL PRODUTO(S)</Td>
-                                                <Td textAlign={'right'}>R$ {valorTotal}</Td>
-                                            </Tr>
-                                            <Tr>
-                                                <Td>DESCONTO</Td>
-                                                <Td textAlign={'right'}>R$ 0</Td>
-                                            </Tr>
-                                        </Tbody>
-                                    </Table>
-                                    </TableContainer>   
-                                </AccordionPanel>
-                            </AccordionItem>
-                            </Accordion>
-                    </div>
-                ))}
-        </Grid>
-        <Footer />
-        </div>
-    )
-}
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <NavBar />
+      <Grid templateRows={`repeat(${pedidos.length}, 1fr)`} gap={4}>
+        <Text mt={"1.4rem"} justifySelf={"flex-start"} fontSize={"2rem"}>
+          <strong>{`${user.nome}`}</strong>
+        </Text>
+        {pedidos.map(({ id, valorTotal, itens }, index) => (
+          <div key={id}>
+            <Accordion allowToggle isLazy onChange={() => handleAccordionClick(index)}>
+              <AccordionItem
+                isDisabled={false}
+                style={{
+                  marginBottom: isAccordionExpanded === index ? "-10px" : "0",
+                  marginTop: isAccordionExpanded === index ? "-10px" : "0",
+                }}
+              >
+                {({ isExpanded }) => (
+                  <>
+                    <AccordionButton style={{ height: "60px", display: "flex" }}>
+                      <Box as="span" flex="1" textAlign="left">
+                        Pedido Nº {id}
+                      </Box>
+                      <Box as="span" flex="1" textAlign="center">
+                        Forma de pagamento:
+                      </Box>
+                      <Box as="span" flex="1" textAlign="right">
+                        Detalhes do pedido
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                    <AccordionPanel pb={4}>
+                      {itens.map(({ idProduto, quantidade }) => {
+                        const produto = produtos.find((produto) => produto.id === idProduto);
 
-export default Pedidos
+                        if (produto) {
+                          return (
+                            <Grid
+                              key={produto.id}
+                              h="150px"
+                              templateRows="repeat(2, 1fr)"
+                              templateColumns="repeat(5, 1fr)"
+                              marginBottom="10px"
+                              gap={3}
+                            >
+                              <GridItem rowSpan={2} colSpan={1}>
+                                <img src={produto.imgurl} alt="Produto" borderRadius="sm" width={"80px"} />
+                              </GridItem>
+                              <GridItem colSpan={4} textAlign="left">
+                                <h2>{produto.nome}</h2>
+                              </GridItem>
+                              <GridItem colSpan={4} textAlign="right">
+                                <h2>R$ {produto.preco}</h2>
+                              </GridItem>
+                            </Grid>
+                          );
+                        }
+                        return null;
+                      })}
+                      <TableContainer>
+                        <Table>
+                          <Tbody>
+                            <Tr>
+                              <Td>TOTAL PRODUTO(S)</Td>
+                              <Td textAlign={"right"}>R$ {valorTotal}</Td>
+                            </Tr>
+                            <Tr>
+                              <Td>DESCONTO</Td>
+                              <Td textAlign={"right"}>R$ 0</Td>
+                            </Tr>
+                          </Tbody>
+                        </Table>
+                      </TableContainer>
+                    </AccordionPanel>
+                  </>
+                )}
+              </AccordionItem>
+            </Accordion>
+          </div>
+        ))}
+      </Grid>
+      <Footer />
+    </div>
+  );
+};
+
+export default Pedidos;
