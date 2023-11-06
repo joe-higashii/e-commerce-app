86872a7a2327ce54b0f87fed4e3d2ad1f5b2ac7c
Joe <joedsonfera@hotmail.com>
2023-11-06 16:57:32 -0300

    feat: logo kraken login redirecionando para página home

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { Flex, Text } from '@chakra-ui/react'
 import { FaGitkraken } from "react-icons/fa";
 import FormularioLogin from "../../components/FormularioLogin/FormularioLogin.jsx"
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+
   return (
     <>
       <Flex flexDir={'column'} alignItems={'center'} justifyContent={'center'}>
       <FaGitkraken fontSize={'4rem'} ></FaGitkraken>
       <Text fontSize={"2rem"} fontWeight={'bold'}>
-          <a href="">
+          <a href="" onClick={navigateHome}>
             <strong>KRAKEN</strong>
           </a>
         </Text>
