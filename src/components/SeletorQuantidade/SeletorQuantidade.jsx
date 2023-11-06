c9d324bc3275d5d305192534e0b1f96ccf2d9021
LeonardoLucas13 <leonardolucas013@gmail.com>
2023-11-06 19:59:16 -0300

    Feat: adicionado varios produtos json
    alteracao do seletor de quantidade

--- src/components/SeletorQuantidade/SeletorQuantidade.jsx
+++ src/components/SeletorQuantidade/SeletorQuantidade.jsx
@@ -4,25 +4,22 @@ import { HStack } from "@chakra-ui/layout"
 import { useNumberInput } from "@chakra-ui/number-input"
 
 
-const SeletorQuantidade = ({ quantidade, totalitens }) => {
-    const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
-        useNumberInput({
-            step: 1,
-            defaultValue: totalitens,
-            min: 1,
-            max: quantidade,
-            precision: 0,
-        })
+const SeletorQuantidade = ({ quantidade, totalitens, setqtd }) => {
+    
+    const handleChangeDecrement = () => {
+        setqtd(totalitens-1)
+    }
 
-    const inc = getIncrementButtonProps()
-    const dec = getDecrementButtonProps()
-    const input = getInputProps()
+    const handleChangeIncrement = () => {
+        setqtd(totalitens+1)
+    }
+    
 
     return (
         <HStack maxW='150px'>
-            <Button {...dec}>-</Button>
-            <Input {...input} />
-            <Button {...inc}>+</Button>
+            <Button onClick={handleChangeDecrement}>-</Button>
+            <Input value={totalitens} onChange={(e) => {setqtd(e.target.value)}}/>
+            <Button onClick={handleChangeIncrement}>+</Button>
         </HStack>
     )
 }
