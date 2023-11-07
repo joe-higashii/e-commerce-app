4515aef4ae4bad251ad3f5b4ee9e436c196365f3
Joe <joedsonfera@hotmail.com>
2023-11-07 19:28:10 -0300

    fix: agora o seletor de quantidade de produtos no carrinho não pode ser superior a quantidade do produto no estoque

--- src/components/SeletorQuantidade/SeletorQuantidade.jsx
+++ src/components/SeletorQuantidade/SeletorQuantidade.jsx
@@ -3,24 +3,27 @@ import { Input } from "@chakra-ui/input"
 import { HStack } from "@chakra-ui/layout"
 import { useNumberInput } from "@chakra-ui/number-input"
 
-
 const SeletorQuantidade = ({ quantidade, totalitens, setqtd }) => {
     
     const handleChangeDecrement = () => {
-        setqtd(totalitens-1)
+        if (totalitens > 1) {
+            setqtd(totalitens - 1);
+        }
     }
 
     const handleChangeIncrement = () => {
-        setqtd(totalitens+1)
+        if (totalitens < quantidade) {
+            setqtd(totalitens + 1);
+        }
     }
-    
 
     return (
         <HStack maxW='150px'>
             <Button onClick={handleChangeDecrement}>-</Button>
-            <Input value={totalitens} onChange={(e) => {setqtd(e.target.value)}}/>
+            <Input value={totalitens} onChange={(e) => {setqtd(parseInt(e.target.value) || 0)}}/>
             <Button onClick={handleChangeIncrement}>+</Button>
         </HStack>
     )
 }
+
 export default SeletorQuantidade
