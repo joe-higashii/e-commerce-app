fab577d5574d158726cfb8867c280aed4859fefd
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-11-06 21:55:08 -0300

    fix: style de Pedidos e Favoritos
    add: Imagens na home
    fix: ternario para requisitar login em Pedidos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,9 +14,9 @@ function Home() {
         gap={4}
         padding={'1rem'}
       >
-        <GridItem h={'700px'} rowSpan={2} colSpan={1} padding={'1rem'} border={'1px solid #D9D9D9'} borderRadius={'12px'} >Produto1</GridItem>
-        <GridItem rowSpan={1} colSpan={1} padding={'1rem'} border={'1px solid #D9D9D9'} borderRadius={'12px'} >Produto2</GridItem>
-        <GridItem rowSpan={1} colSpan={1} padding={'1rem'} border={'1px solid #D9D9D9'} borderRadius={'12px'} >Produto3</GridItem>
+        <GridItem backgroundSize={'cover'} backgroundImage={'https://img.freepik.com/vetores-premium/detalhado-laptop-mockup-vertical-banner-design-tela-realista-vista-lateral-sobre-fundo-escuro-ilustracao-vetorial_541075-946.jpg?w=2000'} h={'700px'} rowSpan={2} colSpan={1} padding={'1rem'} border={'1px solid #D9D9D9'} borderRadius={'12px'} >Produto1</GridItem>
+        <GridItem backgroundSize={'cover'} backgroundRepeat={'no-repeat'} backgroundImage={'https://www.zotac.com/download/files/styles/org/public/news/images/vga_keyart_banner_1920-580_15.jpg?itok=4rtDIKz0'} rowSpan={1} colSpan={1} padding={'1rem'} border={'1px solid #D9D9D9'} borderRadius={'12px'} >Produto2</GridItem>
+        <GridItem backgroundImage={'https://images.anandtech.com/doci/15804/Banner_1920x1080_678x452.jpg'} rowSpan={1} colSpan={1} padding={'1rem'} border={'1px solid #D9D9D9'} borderRadius={'12px'} >Produto3</GridItem>
 
       </Grid>
       <Footer />
